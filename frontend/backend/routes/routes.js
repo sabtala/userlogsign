5a62413b95cfe7b4e5be6fa7d9bf7a8da9900669
Michel Fopa <michelfopa@Michels-MacBook-Pro.local>
2021-01-18 16:24:23 -0600

    install bcrypt and created a salt to encrypt the user password

--- frontend/backend/routes/routes.js
+++ frontend/backend/routes/routes.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const router = express.Router()
 const signUpTemCopy = require("../models/Signup")
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (req, res) =>{
+router.post('/signup', async (req, res) =>{
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
     const signedUpUser = new signUpTemCopy({
         fullName:req.body.fullName,
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password
+        password:securePassword
 
     })
     signedUpUser.save()
